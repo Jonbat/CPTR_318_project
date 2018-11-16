2e6fe0a0eee1cd5924471e5c7dfc812c0365f6ea
Jonathan Batchelder <jonathanbatchelder@southern.edu>
2018-11-16 16:45:57 -0500

    Create event module complete

---
+++ calendar_ui/Assets/Scripts/SaveFunctionality.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveFunctionality : MonoBehaviour {
+
+    public InputField title;
+    public InputField date;
+    public InputField start_time;
+    public InputField end_time;
+
+    public void create_event () {
+
+        string[] new_event_data = { title.text, date.text, start_time.text, end_time.text };
+
+        // TODO: send new_event_data to calendar with protocol
+    }
+}
