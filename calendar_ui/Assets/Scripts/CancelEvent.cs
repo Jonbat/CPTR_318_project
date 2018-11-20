eb3366b312e5d8de69a68d61234211be06eda766
Jonathan Batchelder <jonathanbatchelder@southern.edu>
2018-11-20 11:39:47 -0500

    Cancel button functionality

---
+++ calendar_ui/Assets/Scripts/CancelEvent.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CancelEvent : MonoBehaviour {
+
+	public void Return_to_calendar () {
+        SceneManager.LoadScene("CalendarView");
+    }
+}
